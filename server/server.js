097350bff9302a74748d1e50b2ef26188c5dbe6c
Chandan Kumar <chandan.6278@gmail.com>
2024-05-01 13:38:11 +0530

    server fixes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("../config/db");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -8,10 +9,18 @@ const app = express();
 connectDB();
 app.use(cors());
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 //Define routes
-app.use("/api/cars", require("./routes/api/car"));
+app.use(process.env.API_PREFIX + "/", require("./routes/api/car"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
